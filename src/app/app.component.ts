9b10a812443b353d746b37d101398a4c9649115f
Kevin Jared <kevinjaredkip@gmail.com>
2018-08-09 15:51:48 +0300

    strickethrough

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import {Goal}from './goal'
 
 @Component({
-  selector: 'app-root'
+  selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  
+
 
 }
